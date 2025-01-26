a0934b69d5cbbf4544c49d57bd33dfc585b7ad65
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-01-26 06:22:39 +0200

    generate HTML Template

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
 <?php
 echo "I Love PHP <br>";
-echo "Ahmed";
-// This is a comment
-/*
-    This
-    is 
-    multiline
-    comment
-*/
+echo "Ahmed<br>";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <button>Order A Pizza</button>
+</body>
+
+</html>
