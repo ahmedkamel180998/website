cbc2c651e62b600106a75128b61e088477edffa8
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-01-27 00:01:30 +0200

    Associative Array

--- index.php
+++ index.php
@@ -1,14 +1,37 @@
 <?php
-$foods = array("Apple", "Orange", "Banana", "Coconut");
+/* Associative Array = An array made of Key=>Value PAIRS
+        ex.1/ countries => capitals
+        ex.2/ id => username
+        ex.3/ item => price
+*/
+$countries = array(
+    "USA" => "Washington D.C.",
+    "Japan" => "Kyoto",
+    "South Korea" => "Seoul",
+    "India" => "New Delhi"
+);
 
-//$foods[0] = "Kiwi";
-//array_push($foods, "Pineapple");
-//array_pop($foods);
-//$food = array_shift($foods);
-//echo $food . " shifted<br>";
-// $reversedFoods = array_reverse($foods);
+$countries["USA"] = "Las Vegas";
+$countries["China"] = "Beijing";
 
-// foreach ($reversedFoods as $food) {
-//     echo $food . "<br>";
+//array_pop($countries);
+//array_shift($countries);
+// $keys = array_keys($countries);
+// $values = array_values($countries);
+// foreach ($countries as $key => $value) {
+//     echo "{$key} = {$value}<br>";
 // }
-echo count($foods);
+
+//$countries = array_flip($countries);
+// $countries = array_reverse($countries);
+// foreach ($countries as $key => $value) {
+//     echo "{$key} = {$value}<br>";
+// }
+// foreach ($keys as $key) {
+//     echo "{$key}<br>";
+// }
+// foreach ($values as $value) {
+//     echo "{$value}<br>";
+// }
+
+echo count($countries);
