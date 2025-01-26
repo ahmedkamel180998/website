141a3a8136a747bdf686db55b9fe6031c64a3d20
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-01-26 07:19:44 +0200

    get and post intro

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
-<?php
-// operator precedence
-// ()
-// **
-// * / %
-// + -
+<!DOCTYPE html>
+<html lang="en">
 
-$total = 1 + 2 - 3 * 4 / 5 ** 6;
-echo $total;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+</head>
+
+<body>
+    <form action="index.php" method="get">
+        <label>Username:</label><br>
+        <input type="text" name="userName"><br>
+        <label>Password:</label><br>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Log In"><br>
+    </form>
+</body>
+
+</html>
+<?php
+echo "{$_GET["userName"]} <br>";
+echo "{$_GET["password"]} <br>";
+//echo "{$_POST["userName"]} <br>";
+//echo "{$_POST["password"]} <br>";
+?>
