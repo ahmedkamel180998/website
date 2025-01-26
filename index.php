9f26872ff8fce93a81725c5e9090a8e565584017
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-01-26 23:05:45 +0200

    while loop

--- index.php
+++ index.php
@@ -1,68 +1,61 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Count</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <label>Enter A Number To Count To:</label>
+        <input type="text" name="counter">
+        <input type="submit" value="Start">
+    </form>
+</body>
+
+</html> -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Count</title>
+</head>
+
+<body>
+    <form action="index.php" method="post">
+        <input type="submit" name="stop" value="Stop">
+    </form>
+    <form action="index.php" method="post">
+        <input type="text" name="reset" value=null>
+        <input type="submit" name="reset" value="reset">
+    </form>
+</body>
+
+</html>
 <?php
-/*switch statement
-        1- replacement to using many elseif statements
-        2- more efficient and less code to write
-*/
-$grade = 'F';
-// if ($grade == "A" || $grade == "a") {
-//     echo "You Did Great";
-// } elseif ($grade == "B" || $grade == "b") {
-//     echo "You Did Good";
-// } elseif ($grade == "C" || $grade == "c") {
-//     echo "You Did Okay";
-// } elseif ($grade == "D" || $grade == "d") {
-//     echo "You Did Poorly";
-// } elseif ($grade == "F" || $grade == "f") {
-//     echo "You Failed";
-// } else {
-//     echo "{$grade} is not a vaild grade";
+// $counter = $_POST["counter"];
+// for ($i = 1; $i <= $counter; $i++) {
+//     echo "{$i}<br>";
 // }
-// switch ($grade) {
-//     case 'A':
-//         echo "You Did Great";
-//         break;
-//     case 'B':
-//         echo "You Did Good";
-//         break;
-//     case 'C':
-//         echo "You Did Okay";
-//         break;
-//     case 'D':
-//         echo "You Did Poorly";
-//         break;
-//     case 'F':
-//         echo "You Failed";
-//         break;
-//     default:
-//         echo "{$grade} is not a vaild grade";
-//         break;
+// for ($i = $counter; $i >= 1; $i--) {
+//     echo "{$i}<br>";
+// }
+// for ($i = $counter; $i >= 1; $i -= 3.5) {
+//     echo "{$i}<br>";
+// }
 
-$date = date("l");
-$date = "Pizza";
-
-switch ($date) {
-    case "Monday":
-        echo "I hate mondays";
-        break;
-    case "Tuesday":
-        echo "It's a taco day";
-        break;
-    case "Wednesday":
-        echo "The work week is half over";
-        break;
-    case "Thursday":
-        echo "It's almost the weekend";
-        break;
-    case "Friday":
-        echo "The weekend is here";
-        break;
-    case "Saturday":
-        echo "Time to party";
-        break;
-    case "Sunday":
-        echo "Time to Relax";
-        break;
-    default:
-        echo "{$date} is not a vaild day";
-        break;
+$running = true;
+$seconds = 1;
+while ($running) {
+    if (isset($_POST["stop"])) {
+        $running = false;
+    } else {
+        $seconds++;
+        echo "{$seconds}<br>";
+    }
 }
+?>
