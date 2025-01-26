4d8af16a3e1979eb1660c92533e9dc6a93f7fb4a
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-01-26 06:23:45 +0200

    variables and data types

--- index.php
+++ index.php
@@ -1,18 +1,34 @@
 <?php
-echo "I Love PHP <br>";
-echo "Ahmed<br>";
-?>
-<!DOCTYPE html>
-<html lang="en">
+$name = "Ahmed";
+$food = "Pizza";
+$email = "fake@gmail.com";
+// integers
+$age = 26;
+$users = 2;
+$quantity = 3;
+// floats
+$gpa = 3.14;
+$price = 4.99;
+$taxRate = 2.1;
+// boleans
+$employed = true;
+$online = false;
+$forSale = false;
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+echo "Hello {$name}<br>";
+echo "You Like {$food}<br>";
+echo "Your Email is: {$email}<br>";
+echo "Your Age is: {$age} Years Old<br>";
+echo "There are {$users} users online<br>";
+echo "You Would Like To Buy {$quantity} Items<br>";
+echo "Your GPA is: {$gpa}<br>";
+echo "Your Pizza is: \${$price}<br>";
+echo "The Sales Tax Rate is: {$taxRate}%<br>";
+echo "Online Status is: {$online}<br>";
+echo "Employed Status is: {$employed}<br>";
+echo "For Sale is: {$forSale}<br>";
 
-<body>
-    <button>Order A Pizza</button>
-</body>
-
-</html>
+$total = null;
+echo "You have ordered {$quantity}x {$food}s<br>";
+$total = $quantity * $price;
+echo "Total price is: \${$total}<br>";
