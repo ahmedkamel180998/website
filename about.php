adaf6c1e812eb1a094473c74102ca713ff62e99f
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-01-27 03:06:23 +0200

    include() in PHP

--- about.php
+++ about.php
@@ -7,11 +7,11 @@ include("header.html");
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>About</title>
 </head>
 
 <body>
-    <h1>This is Home Page</h1>
+    <h1>This is About Page</h1>
 </body>
 
 </html>
