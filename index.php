4a70b326d3f640ea36dea375d79d462e63eda638
Ahmed Kamel <ahmedkamel180998@gmail.com>
2025-01-27 02:13:09 +0200

    string functions in php

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <?php
-function hypotenuse(float $a, float $b)
-{
-    $c = sqrt($a ** 2 + $b ** 2);
-    return $c;
-}
+$username = "Bro Code";
+$phone = "123-456-7890";
 
-echo round(hypotenuse(3, 4), 2);
+//$username = strtolower($username);
+//$username = strtoupper($username);
+//$username = trim($username);
+//$username = str_pad($username, 20, "0");
+//$phone = str_replace("-", "//", $phone);
+//$username = strrev($username);
+//$username = str_shuffle($username);
+//$username = strcmp($username, "Bro Cod");
+//$count = strlen($username);
+//$index = strpos($username, " ");
+//$firstName = substr($username, 0, 3);
+//$lastName = substr($username, 4, 8);
+
+// $fullName = explode(" ", $username);
+// foreach ($fullName as $value) {
+//     echo $value . "<br>";
+// }
+$name = array("Bro", "The", "Code");
+$name =  implode("^-^", $name);
+echo $name;
